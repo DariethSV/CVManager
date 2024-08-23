41c9c674d1761ff7b4840f8b11ae7ef11af8fc9b
Darieth <darieth.s.v@gmail.com>
2024-08-23 00:09:08 -0500

    Implementación básica de la extensión de navegador

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.getElementById('dataForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    fetch('http://localhost:8000/api/save-data/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name, email: email })
+    })
+    .then(response => response.json())  
+    .then(data => {
+        console.log(data.message); 
+    })
+    .catch(error => {
+        console.error('Error:', error); 
+    });
+});
