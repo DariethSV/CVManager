9f611d63365251705abf76af92def1833b046418
Santiago Sabogal Lozano <ssabogall@eafit.edu.co>
2024-09-06 13:04:45 -0500

    commit inicial del repositorio

---
+++ content.js
@@ -0,0 +1,62 @@
+function detect_forms() {
+    console.log('Iniciando la detección de formularios');
+    const forms = document.querySelectorAll('form');
+    console.log(`Número de formularios encontrados: ${forms.length}`);
+
+    forms.forEach(form => {
+        const inputs = form.querySelectorAll('input:not([type="hidden"]), textarea, select');
+        console.log(`Número de inputs encontrados en el formulario: ${inputs.length}`);
+
+        inputs.forEach(input => {
+
+            if (input.parentNode.querySelector('.custom-container')) {
+                return; 
+            }
+
+            const container = document.createElement('div');
+            container.classList.add('custom-container');
+            container.style.display = 'flex';
+            container.style.alignItems = 'center';
+            container.style.marginBottom = '10px'; 
+
+            const button = document.createElement('span');
+            button.textContent = 'Autocompletar';
+            button.style.marginLeft = '10px'; 
+            button.style.cursor = 'pointer';
+            button.style.color = 'blue';
+            button.style.textDecoration = 'underline';
+
+            button.onclick = () => fill_input(input);
+
+            input.parentNode.insertBefore(container, input);
+
+            container.appendChild(input);
+
+            container.appendChild(button);
+        });
+    });
+}
+
+function fill_input(input) {
+    fetch('http://localhost:8000/api/get_data/')
+        .then(response => response.json())
+        .then(data => {
+            const resume_data = data.resume_data[0]; // Obtener el primer objeto del array
+            if (resume_data) {
+                const key = (input.name).toLowerCase(); // Usar name o id del input para buscar en los datos del servidor
+                if (key && resume_data[key] !== undefined) {
+                    // Si existe un valor para la key en los datos del servidor
+                    input.value = resume_data[key];
+                } else {
+                    alert(`No se encontró un valor para "${key}" en los datos.`);
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Hubo un error al cargar los datos. Por favor, intenta de nuevo.');
+        });
+}
+
+// Llama a la función para detectar formularios
+detect_forms();
