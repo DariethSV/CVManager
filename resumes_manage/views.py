b5fc4e28d37bbcd6b811cf222197ad82fbbc131a
Darieth <darieth.s.v@gmail.com>
2024-09-23 13:15:43 -0500

    Save resumes basic implementation

--- resumes_manage/views.py
+++ resumes_manage/views.py
@@ -5,111 +5,67 @@ from django.contrib.auth.decorators import login_required
 import json
 from .models import *
 from django.shortcuts import render
-
+from django.contrib import messages
+from access.models import Customer
 
 @login_required
 @require_POST
 @csrf_exempt
 def save_resume(request):
+    user = request.user
+    
     if request.method == 'POST':
-        data = json.loads(request.body)
+        customer = Customer.objects.get(email=user.email)
+        full_name = request.POST.get('full_name')
+        birth_date = request.POST.get('birth_date')
+        resume_email = request.POST.get('resume_email')
+        phone_number = request.POST.get('phone_number')
+        professional_summary = request.POST.get('professional_summary')
+        company_name = request.POST.get('company_name')
+        position = request.POST.get('position')
+        start_date = request.POST.get('start_date')
+        description = request.POST.get('description')
+        degree = request.POST.get('degree')
+        institution = request.POST.get('institution')
+        start_date_education = request.POST.get('start_date_education')
+        end_date_education = request.POST.get('end_date_education')
+        description_education = request.POST.get('description_education')
+        skills = request.POST.get('skills')
+        proficiency_level = request.POST.get('proficiency_level')
+        language = request.POST.get('language')
+        fluency = request.POST.get('fluency')
+        project_name = request.POST.get('project_name')
+        description_project = request.POST.get('description_project')
+        title = request.POST.get('title')
+        institution = request.POST.get('institution')
+        date_obtained = request.POST.get('date_obtained')
+        reference_name = request.POST.get('reference_name')
+        relationship = request.POST.get('relationship')
+        contact_info = request.POST.get('contact_info')
 
-        # Validar información personal
-        if not data.get('full_name') or not data.get('birth_date') or not data.get('resume_email') or not data.get('phone_number') or not data.get('professional_summary'):
-            return JsonResponse({'error': 'Por favor, llene todos los campos de información personal'}, status=400)
+        print("FLUENCY LEVEL: ")
+        print(fluency)
+
+
 
         resume = Resume(
-            full_name=data['full_name'],
-            birth_date=data['birth_date'],
-            resume_email=data['resume_email'],
-            phone_number=data['phone_number'],
-            professional_summary=data['professional_summary']
+            customer=customer,
+            full_name=full_name,
+            birth_date=birth_date,
+            resume_email=resume_email,
+            phone_number=phone_number,
+            professional_summary=professional_summary
         )
-        resume.save()
+        resume.save()       
+        messages.success(request, 'Hoja de vida creada exitosamente')
+        return render(request, 'create_resume.html')
 
-        # Procesar experiencia laboral
-        work_experiences = data.get('work_experiences', [])
-        for experience in work_experiences:
-            work_experience = WorkExperience(
-                resume=resume,
-                company_name=experience['company_name'],
-                position=experience['position'],
-                start_date=experience['start_date'],
-                end_date=experience['end_date'],
-                description=experience['description']
-            )
-            work_experience.save()  # Guardar cada objeto WorkExperience
-
-        # Procesar educación
-        educations = data.get('educations', [])
-        for edu in educations:
-            education = Education(
-                resume=resume,
-                degree=edu['degree'],
-                institution=edu['institution'],
-                start_date=edu['start_date'],
-                end_date=edu['end_date'],
-                description=edu['description']
-            )
-            education.save()  # Guardar cada objeto Education
-
-        # Procesar habilidades
-        skills = data.get('skills', [])
-        for skill in skills:
-            skill_obj = Skill(
-                resume=resume,
-                skill_name=skill['skill_name'],
-                proficiency_level=skill['proficiency_level']
-            )
-            skill_obj.save()  # Guardar cada objeto Skill
-
-        # Procesar idiomas
-        languages = data.get('languages', [])
-        for lang in languages:
-            language = Language(
-                resume=resume,
-                language=lang['language'],
-                fluency=lang['fluency']
-            )
-            language.save()  # Guardar cada objeto Language
-
-        # Procesar proyectos
-        projects = data.get('projects', [])
-        for project in projects:
-            project_obj = Project(
-                resume=resume,
-                project_name=project['project_name'],
-                description=project['description'],
-                technologies_used=project['technologies_used']
-            )
-            project_obj.save()  # Guardar cada objeto Project
-
-        # Procesar certificaciones
-        certifications = data.get('certifications', [])
-        for cert in certifications:
-            certification = Certification(
-                resume=resume,
-                title=cert['title'],
-                institution=cert['institution'],
-                date_obtained=cert['date_obtained']
-            )
-            certification.save()  # Guardar cada objeto Certification
-
-        # Procesar referencias
-        references = data.get('references', [])
-        for reference in references:
-            reference_obj = Reference(
-                resume=resume,
-                reference_name=reference['reference_name'],
-                relationship=reference['relationship'],
-                contact_info=reference['contact_info']
-            )
-            reference_obj.save()  # Guardar cada objeto Reference
-
-        return JsonResponse({'success': 'Hoja de vida guardada exitosamente'})
-
+#
     else:
-        return JsonResponse({'error': 'Error en la decodificación JSON'}, status=400)
+        messages.error(request, 'Authentication failed. Please check your credentials.')
+        return render(request, 'create_resume.html')
+    
+        
     
     
 def create_resume(request):
